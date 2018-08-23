91c882d5839d042faa13a5a8e571681724b387c9
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-22 17:47:10 -0700

    added rolls

--- js/data.js
+++ js/data.js
@@ -17,7 +17,61 @@ window.deposits = [
     description: '',
     value: 5,
     count: 0
-  }
+  },
+  {
+    name: '$10',
+    description: '',
+    value: 10,
+    count: 0
+  },
+  {
+    name: '$20',
+    description: '',
+    value: 20,
+    count: 0
+  },
+  {
+    name: '$50',
+    description: '',
+    value: 50,
+    count: 0
+  },
+  {
+    name: '$100',
+    description: '',
+    value: 100,
+    count: 0
+  },
+  {
+    name: 'RNx$2',
+    description: 'RN: Roll of Nickel',
+    value: 2,
+    count: 0
+  },
+  {
+    name: 'RDx$5',
+    description: 'RD: Roll of Dime',
+    value: 5,
+    count: 0
+  },
+  {
+    name: 'RQx$10',
+    description: 'RQ: Roll of Quarter',
+    value: 10,
+    count: 0
+  },
+  {
+    name: 'RLx$25',
+    description: 'RL: Roll of Loonie',
+    value: 25,
+    count: 0
+  },
+  {
+    name: 'RTx$50',
+    description: 'RT: Roll of Toonie',
+    value: 50,
+    count: 0
+  },
 ];
 
 // Table headers
